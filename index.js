5daf48c04cc2a0d6b8127b27258d2f01e4397595
felixateya <mulaafelix6@gmail.com>
2024-06-07 15:13:06 +0300

    restructured all my apis

--- index.js
+++ index.js
@@ -1,78 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
 
 dotenv.config();
 
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Starting the server
+// routes
+app.use("/api/products", productRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
 });
-
-// create a new product
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// fetch all products
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// fetch product by id
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// update a product
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json({ message: updatedProduct})
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// delete a product
-app.delete("/api/product/:id", async (req, res) => {
-  try{
-    const {id} = req.params
-    const product = await Product.findByIdAndDelete(id)
-    if(!product){
-      return res.status(404).json({message: "Product not found"})
-    }
-    res.status(200).json({message: "Product deleted successfully"})
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-})
 
 // connectiing to MongoDB
 mongoose
---
+++ routes/product.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router()
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller');
+
+router.get("/",getProducts)
+router.get("/:id",getProduct)
+
+// create a product
+router.post("/", createProduct)
+
+// update a product
+router.put("/:id",updateProduct)
+
+// delete a product
+router.delete("/:id",deleteProduct)
+
+module.exports = router
